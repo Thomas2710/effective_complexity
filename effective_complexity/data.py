Fixed: plot scaling, Added: Variance explained

effective_complexity/data.py
@@ -11,6 +11,7 @@ import os
 
 matplotlib.use('Qt5Agg')  # Use an interactive backend like TkAgg
 
+#Show syntethic gaussian distribution
 def show_gaussian(samples, reference = True):
     if reference:
         x, y = np.mgrid[-1.0:1.0:30j, -1.0:1.0:30j]
@@ -42,57 +43,59 @@ def show_gaussian(samples, reference = True):
     plt.legend()
     plt.show()
 
-def show_distrib(distrib, method = 'NO', predicted = True, ax = None):
+#Plot distribution in input, in plt ax in input
+def show_distrib(distrib, method = 'NO', predicted = True, ax = None, limit=None):
+    min_limit, max_limit = limit
     dim = distrib.shape[1]
     if predicted:
         exp = 'predicted'
     else:
         exp = 'reference'
-    '''
-    # Plotting 2D data
-    if dim == 2:
-        plt.figure(figsize=(8, 6))
-        plt.scatter(distrib[:, 0], distrib[:, 1], c='blue', alpha=0.6)
-        plt.title(f"{method.upper()} Reduced Visualization of {exp} Data (2D)") 
-        plt.xlabel("Dimension 1")
-        plt.ylabel("Dimension 2")
-    
-    # Plotting 3D data
-    elif dim == 3:
-        fig = plt.figure(figsize=(8, 6))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(distrib[:, 0], distrib[:, 1], distrib[:, 2], c='blue', alpha=0.6)
-        ax.set_title(f"{method.upper()} Reduced Visualization of {exp} Data (3D)")
-        ax.set_xlabel("Dimension 1")
-        ax.set_ylabel("Dimension 2")
-        ax.set_zlabel("Dimension 3")
-    else:
-        raise ValueError("target_dim must be 2 or 3.")
-    '''
+
     if dim == 2:
         ax.scatter(distrib[:, 0], distrib[:, 1], c='blue', alpha=0.6)
         ax.set_title(f"{method.upper()} Reduced Visualization of {exp} Data (2D)")
         ax.set_xlabel("Dimension 1")
         ax.set_ylabel("Dimension 2")
+        ax.set_xlim(min_limit[0],max_limit[0])
+        ax.set_ylim(min_limit[1],max_limit[1])
     elif dim == 3:
         ax.scatter(distrib[:, 0], distrib[:, 1], distrib[:, 2], c='blue', alpha=0.6)
         ax.set_title(f"{method.upper()} Reduced Visualization of {exp} Data (3D)")
         ax.set_xlabel("Dimension 1")
         ax.set_ylabel("Dimension 2")
         ax.set_zlabel("Dimension 3")
+        ax.set_xlim(min_limit[0],max_limit[0])
+        ax.set_ylim(min_limit[1],max_limit[1])
+        ax.set_zlim(min_limit[2],max_limit[2])
     else:
         raise ValueError("target_dim must be 2 or 3.")
 
+#Find the range of two distributions in each dimension
+def find_distribution_limits(distrib1, distrib2):
+    # Find min and max per dimension (i.e., column)
+    min_per_dimension = np.min(distrib1, axis=0)
+    max_per_dimension = np.max(distrib2, axis=0)
 
+    min_per_pred_dimension = np.min(distrib1, axis=0)
+    max_per_pred_dimension = np.max(distrib2, axis=0)
 
+    min_limit = np.minimum(min_per_dimension, min_per_pred_dimension)
+    max_limit = np.maximum(max_per_dimension, max_per_pred_dimension)
+    limit = (min_limit, max_limit)
+    return limit
 
+#Apply PCA dim reduction to distribution
 def apply_pca(data, num_components=2):
     # Apply PCA to reduce to 2 dimensions
     pca = PCA(n_components=num_components)
     pca_result = pca.fit_transform(data)
     data_reconstructed = pca.inverse_transform(pca_result)
-    return pca_result, data_reconstructed
+    variance_explained = np.cumsum(pca.explained_variance_ratio_)
 
+    return pca_result, data_reconstructed, variance_explained
+
+#Apply t-SNE dim reduction to distribution
 def apply_tsne(data, num_components=2, perplexity=30, random_state=42):
     # Apply t-SNE to reduce to 2 dimensions for visualization
     tsne = TSNE(n_components=num_components, perplexity=perplexity, random_state=random_state)

effective_complexity/functions.py
@@ -11,16 +11,17 @@ l2_lambda = 1e-4
 l1_weight = 0
 l2_weight = 0
 
+#Elastic regularization, allows to customize the weight of L1 and L2 regularization
 def elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight):
     l1_penalty = sum(p.abs().sum() for p in model.parameters())  # L1 norm
     l2_penalty = sum((p ** 2).sum() for p in model.parameters())  # L2 norm
     return l1_weight * l1_lambda * l1_penalty + l2_weight * l2_lambda * l2_penalty
 
-
+#Train loop
 def train_loop(train_loader, model, criterion, optimizer, device = 'cpu'):
     model.train()
     logsoftmax = nn.LogSoftmax(dim=-1)
-    total_loss = 0
+    total_loss = []
     accuracy = 0
     total = 0
     correct = 0
@@ -40,31 +41,28 @@ def train_loop(train_loader, model, criterion, optimizer, device = 'cpu'):
         unembedding = model.get_unembeddings(one_hots)
         logits = torch.matmul(f_x, unembedding)
 
-
-
         outputs = logsoftmax(logits)
-        #MINIMIZE KL DIVERGENCE
         loss = criterion(outputs, labels)
-        loss += elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight)
+        #loss += elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight)
 
-        total_loss += loss.item()
+        total_loss.append(loss.item())
+        '''
         #Accuracy computation
-        #_, predicted = torch.max(outputs, 1)
-        #correct += (predicted == labels).sum().item()
-        #total += labels.size(0)
+        _, predicted = torch.max(outputs, 1)
+        correct += (predicted == labels).sum().item()
+        total += labels.size(0)'
+        '''
 
-        # Backward pass
         loss.backward()
         optimizer.step()
 
-    avg_loss = total_loss / len(train_loader)
-    #accuracy = correct / total
+    avg_loss = sum(total_loss) / len(total_loss)
     return avg_loss, accuracy
 
 
-
+#Validation loop
 def val_loop(val_loader, model, criterion, device = 'cpu'):
-    model.eval()  # Set model to evaluation mode (no gradient computation)
+    model.eval()
     total_loss = 0.0
     accuracy = 0
     total = 0
@@ -75,12 +73,10 @@ def val_loop(val_loader, model, criterion, device = 'cpu'):
     one_hots = torch.eye(d).float()
 
 
-
-    with torch.no_grad():  # No need to track gradients
+    with torch.no_grad(): 
         for batch in val_loader:
             inputs = batch['x'].to(device)
             labels = batch['label'].to(device)
-
             outputs = model(inputs)
 
             #Compute f_x from output
@@ -89,26 +85,25 @@ def val_loop(val_loader, model, criterion, device = 'cpu'):
             unembedding = model.get_unembeddings(one_hots)
 
             logits = torch.matmul(f_x, unembedding)
-
-
             outputs = logsoftmax(logits)
             loss = criterion(outputs, labels)
-            loss += elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight)
+            #loss += elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight)
 
 
             total_loss += loss.item()
+            '''
             #Accuracy computation
-            #_, predicted = torch.max(outputs, 1)
-            #correct += (predicted == labels).sum().item()
-            #total += labels.size(0)
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)'
+            '''
 
     avg_loss = total_loss / len(val_loader)
-    #accuracy = correct / total
     return avg_loss, accuracy
 
-
+#Test loop
 def test_loop(test_loader, model, criterion, device='cpu'):
-    model.eval()  # Set model to evaluation mode (no gradient computation)
+    model.eval()  
     total_loss = 0.0
     accuracy = 0
     total = 0
@@ -122,7 +117,7 @@ def test_loop(test_loader, model, criterion, device='cpu'):
     one_hots = torch.eye(d).float()
 
 
-    with torch.no_grad():  # No need to track gradients
+    with torch.no_grad():
         for batch in test_loader:
             inputs = batch['x'].to(device)
             labels = batch['label'].to(device)
@@ -139,15 +134,14 @@ def test_loop(test_loader, model, criterion, device='cpu'):
             outputs_to_return.append(softmax(logits))
 
             loss = criterion(outputs, labels)
-            loss += elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight)
-
-
+            #loss += elastic_net_regularization(model, l1_lambda, l2_lambda, l1_weight, l2_weight)
             total_loss += loss.item()
+            '''
             #Accuracy computation
-            #_, predicted = torch.max(outputs, 1)
-            #correct += (predicted == labels).sum().item()
-            #total += labels.size(0)
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+            '''
 
     avg_loss = total_loss / len(test_loader)
-    #accuracy = correct / total
     return avg_loss, accuracy, total_fx, outputs_to_return

effective_complexity/main.py
@@ -1,37 +1,42 @@
 import matplotlib.pyplot as plt
-from effective_complexity.data import apply_pca, apply_tsne, show_distrib, show_gaussian
+from effective_complexity.data import apply_pca, apply_tsne, show_distrib, show_gaussian, find_distribution_limits
 from effective_complexity.datasets import collate_fn
 import numpy as np
-#from effective_complexity.model import MLP, initialize_weights
 import torch.nn as nn
 import torch
 from tqdm import tqdm
 import torch.optim as optim
 import os
 from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
 
 from effective_complexity.functions import train_loop, val_loop, test_loop
 from torch.utils.data import DataLoader
 
 
-def identify(dataloaders, model, hyperparams, train):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    #train_dataset, val_dataset, test_dataset = dataset
+
+
+def identify(dataloaders, model, hyperparams, train):
+
+    #------------------------------
+    # Problem initialization
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     train_loader, val_loader, test_loader = dataloaders
     general_hyperparams, model_hyperparams, dataset_hyperparams = hyperparams
 
-    #CREATE SYNTHETIC DATASET
     epochs = general_hyperparams['num_epochs']
     lr = general_hyperparams['learning_rate']
     embedding_size = general_hyperparams['embedding_size']
+    num_classes = general_hyperparams['num_classes']
 
     model_name = model.__class__.__name__
     dataset_name = train_loader.dataset.__class__.__name__
     folder_name = ''+dataset_name+'_'+model_name+'_'+datetime.now().strftime("%Y-%m-%d %H:%M")
+    scaler = MinMaxScaler()
 
-    
-    # Initialize training 
+    #------------------------------
+    # Training initialization
     test_model = model
     criterion = nn.KLDivLoss(size_average=None, reduce=None, reduction='batchmean', log_target=False)
     optimizer = optim.Adam(test_model.parameters(), lr=lr)
@@ -40,6 +45,8 @@ def identify(dataloaders, model, hyperparams, train):
     checkpoints_folder_path = os.path.join(os.getcwd(), 'CHECKPOINTS', folder_name)
     os.makedirs(checkpoints_folder_path, exist_ok=True)
 
+    #------------------------------
+    # Load model if not training
     if not train:
         print('Testing...')
         model_path = os.path.join(checkpoints_folder_path, 'best_model.pth')
@@ -47,15 +54,19 @@ def identify(dataloaders, model, hyperparams, train):
             print(f"No model found at {model_path}.")
             return
         test_model.to(device)
+    #------------------------------
+    # Train/Test Loop
     else:
-        # Print epoch loss
         print('Training ...')
-        losses_over_epochs = []
+        val_loss_over_epochs = []
+        train_loss_over_epochs = []
         progress_bar = tqdm(range(epochs))
         for epoch in progress_bar:
             train_loss, train_acc = train_loop(train_loader,test_model, criterion, optimizer, device)
             val_loss, val_acc = val_loop(val_loader, test_model, criterion, device)
 
+            val_loss_over_epochs.append(val_loss)
+            train_loss_over_epochs.append(train_loss)
             progress_bar.set_postfix(Epoch = f"[{epoch+1}/{epochs}]")
             progress_bar.set_postfix(Train = f"Loss: {train_loss:.4f}, Acc: {train_acc:.4f}" )
             progress_bar.set_postfix(Val = f"Loss: {val_loss:.4f}, Acc: {val_acc:.4f}")
@@ -69,69 +80,159 @@ def identify(dataloaders, model, hyperparams, train):
     print("\nFinal Test Results")
     print(f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
     
-    #PLOT WITH PCA AND TSNE (WHAT DO I PLOT?)
-    # Number of principal components to test
-    num_components = np.arange(1, embedding_size + 1)
-    errors = []
 
-
-
+    #------------------------------
+    # Merge embeddings for each batch
     embeddings = torch.cat(embeddings)
     predicted_distrib = torch.cat(predicted_distrib)
-    # Compute reconstruction error for different numbers of components
-    for n in num_components:
-        pcareduced_distrib, reconstructed_data = apply_pca(embeddings, num_components=n)
-        reconstruction_error = np.mean((embeddings.cpu().numpy() - reconstructed_data) ** 2)
-        errors.append(reconstruction_error)
 
 
-
-    
+    #------------------------------
+    # Create folder to save plots
     plots_folder_path = os.path.join(os.getcwd(), 'PLOTS', folder_name)
     os.makedirs(plots_folder_path, exist_ok=True)
 
 
+    #-----------------------------
+    # Plot Train and Validation Loss over epochs
+    plt.plot(range(1, len(train_loss_over_epochs)+1),train_loss_over_epochs,  label='Train Loss')
+    plt.plot(range(1, len(val_loss_over_epochs)+1),val_loss_over_epochs, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Train and Validation Loss over Epochs')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(plots_folder_path, 'Loss_over_epochs.png'))
+    plt.close()
+
+
+    #-----------------------------
     # Plot Reconstruction Error vs. Number of Components
+    num_components = np.arange(1, embedding_size + 1)
+    errors = []
+    variance = []
+
+    # Compute reconstruction error for different numbers of components
+    for n in num_components:
+        pcareduced_distrib, reconstructed_data, variance_explained = apply_pca(embeddings, num_components=n)
+        reconstruction_error = np.mean((embeddings.cpu().numpy() - reconstructed_data) ** 2)
+        errors.append(reconstruction_error)
+        variance.append(variance_explained[-1])
+
+
     plt.figure(figsize=(8, 5))
-    plt.plot(num_components, errors, marker='o', linestyle='-')
+    plt.plot(num_components, errors, color='red', marker='o', linestyle='-')
     plt.xlabel("Number of Principal Components")
     plt.ylabel("Reconstruction Error (MSE)")
     plt.title("PCA Reconstruction Error vs. Number of Components")
     plt.grid(True)
-    plt.savefig(os.path.join(plots_folder_path, 'PCA_analysis.png'))
-    #plt.show()
+    plt.savefig(os.path.join(plots_folder_path, 'PCA_MSE_analysis.png'))
     plt.close()
 
-    real_distrib = torch.cat([batch['label'] for batch in test_loader])
+    #-----------------------------
+    #Plot Variance explained vs. Number Of Components
+    plt.figure(figsize=(8, 5))
+    plt.plot(num_components, variance, color='green', marker='o', linestyle='-')
+    plt.xlabel("Number of Principal Components")
+    plt.ylabel("Cumulative Variance Explained")
+    plt.title("PCA Variance Explained vs. Number of Components")
+    plt.grid(True)
+    plt.savefig(os.path.join(plots_folder_path, 'PCA_VAR_analysis.png'))
+    plt.close()
+
+    #-----------------------------
+    # Plot Embeddings in 2D and 3D
+    reference_embeddings = test_loader.dataset.f_x
+
+    embeddings = scaler.fit_transform(embeddings)
+    reference_embeddings = scaler.fit_transform(reference_embeddings)
+
     min_index = errors[:3].index(min(errors[:3]))
-    print('num optimal dimensions:', min_index + 1)
-    if min_index < 4 and min_index>1: #Must be
+    print('num optimal dimensions for embedding:', min_index + 1)
+    if min_index < 4 and min_index > 1:
         num_components = min_index + 1
-        pcareduced_pred_distrib, reconstructed_data = apply_pca(predicted_distrib, num_components=num_components)
-        tsnereduced_pred_distrib = apply_tsne(predicted_distrib, num_components=num_components)
-
-        pcareduced_distrib, _ = apply_pca(real_distrib, num_components=num_components)
-        tsnereduced_distrib = apply_tsne(real_distrib, num_components=num_components)
-
         projection = '3d' if num_components == 3 else None
+        if embeddings.shape[1] > 3:
+            #PCA
+            pcareduced_pred_embeddings, reconstructed_data, _ = apply_pca(embeddings, num_components=num_components)
+            pcareduced_embeddings, _ , _ = apply_pca(reference_embeddings, num_components=num_components)
+            pca_fig, pca_axs = plt.subplots(1, 2, figsize=(12, 10),
+                            subplot_kw={'projection': projection} if projection else {})
+            limit = find_distribution_limits(pcareduced_embeddings, pcareduced_pred_embeddings)
+            show_distrib(pcareduced_embeddings, method='PCA', predicted=False, ax=pca_axs[0], limit = limit)
+            show_distrib(pcareduced_pred_embeddings, method='PCA', predicted=True, ax=pca_axs[1], limit = limit)
+            pca_fig.tight_layout()
+            plt.savefig(os.path.join(plots_folder_path,''+str(pcareduced_embeddings.shape[1])+'dim_embeddings_pca.png'))
+            plt.close()
 
+            #TSNE
+            tsnereduced_pred_embeddings = apply_tsne(embeddings, num_components=num_components)
+            tsnereduced_embeddings = apply_tsne(reference_embeddings, num_components=num_components)
+            tsne_fig, tsne_axs = plt.subplots(1, 2, figsize=(12, 10),
+                        subplot_kw={'projection': projection} if projection else {})
+            limit = find_distribution_limits(tsnereduced_embeddings, tsnereduced_pred_embeddings)
+            show_distrib(tsnereduced_embeddings, method='TSNE', predicted=False, ax=tsne_axs[0], limit = limit)
+            show_distrib(tsnereduced_pred_embeddings, method='TSNE', predicted=True, ax=tsne_axs[1], limit = limit)
+            tsne_fig.tight_layout()
+            plt.savefig(os.path.join(plots_folder_path,''+str(tsnereduced_embeddings.shape[1])+'dim_embeddings_tsne.png'))
+            plt.close()
+        else:
+            pred_embeddings = embeddings
+            reference_embeddings = reference_embeddings.cpu().detach().numpy()
+            tsne_fig, tsne_axs = plt.subplots(1, 2, figsize=(12, 10),
+                        subplot_kw={'projection': projection} if projection else {})
+            limit = find_distribution_limits(tsnereduced_embeddings, tsnereduced_pred_embeddings)
+            show_distrib(reference_embeddings, predicted=False, ax=tsne_axs[0], limit = limit)
+            show_distrib(pred_embeddings, predicted=True, ax=tsne_axs[1], limit = limit)
+            tsne_fig.tight_layout()
+            plt.savefig(os.path.join(plots_folder_path,''+str(reference_embeddings.shape[1])+'dim_embeddings.png'))
+            plt.close()
+
+
+
+    #-----------------------------
+    # Plot PCA and t-SNE distributions
+    real_distrib = torch.cat([batch['label'] for batch in test_loader])
+    projection = '3d' if num_classes == 3 else None
+
+    real_distrib = scaler.fit_transform(real_distrib)
+    predicted_distrib = scaler.fit_transform(predicted_distrib)
+    if num_classes > 3:
+        #PCA
+        pcareduced_pred_distrib, reconstructed_data, _ = apply_pca(predicted_distrib, num_components=num_components)
+        pcareduced_distrib, _, _ = apply_pca(real_distrib, num_components=num_components)
         pca_fig, pca_axs = plt.subplots(1, 2, figsize=(12, 10),
                             subplot_kw={'projection': projection} if projection else {})
-
-        show_distrib(pcareduced_distrib, method='PCA', predicted=False, ax=pca_axs[0])
-        show_distrib(pcareduced_pred_distrib, method='PCA', predicted=True, ax=pca_axs[1])
+        limit = find_distribution_limits(pcareduced_distrib, pcareduced_pred_distrib)
+        show_distrib(pcareduced_distrib, method='PCA', predicted=False, ax=pca_axs[0], limit = limit)
+        show_distrib(pcareduced_pred_distrib, method='PCA', predicted=True, ax=pca_axs[1], limit = limit)
         pca_fig.tight_layout()
-        plt.savefig(os.path.join(plots_folder_path,''+str(pcareduced_distrib.shape[1])+'dim_pca.png'))
+        plt.savefig(os.path.join(plots_folder_path,''+str(pcareduced_distrib.shape[1])+'dim_distribution_pca.png'))
         plt.close()
 
-
+        #TSNE
+        tsnereduced_pred_distrib = apply_tsne(predicted_distrib, num_components=num_components)
+        tsnereduced_distrib = apply_tsne(real_distrib, num_components=num_components)
         tsne_fig, tsne_axs = plt.subplots(1, 2, figsize=(12, 10),
                     subplot_kw={'projection': projection} if projection else {})
-        show_distrib(tsnereduced_distrib, method='TSNE', predicted=False, ax=tsne_axs[0])
-        show_distrib(tsnereduced_pred_distrib, method='TSNE', predicted=True, ax=tsne_axs[1])
+        limit = find_distribution_limits(tsnereduced_distrib, tsnereduced_pred_distrib)
+        show_distrib(tsnereduced_distrib, method='TSNE', predicted=False, ax=tsne_axs[0], limit = limit)
+        show_distrib(tsnereduced_pred_distrib, method='TSNE', predicted=True, ax=tsne_axs[1], limit = limit)
         tsne_fig.tight_layout()
-        plt.savefig(os.path.join(plots_folder_path,''+str(tsnereduced_distrib.shape[1])+'dim_tsne.png'))
+        plt.savefig(os.path.join(plots_folder_path,''+str(tsnereduced_distrib.shape[1])+'dim_distribution_tsne.png'))
         plt.close()
+    else:
+        fig, axs = plt.subplots(1, 2, figsize=(12, 10),
+                            subplot_kw={'projection': projection} if projection else {})
+        real_distrib = real_distrib
+        predicted_distrib = predicted_distrib
+        limit = find_distribution_limits(real_distrib, predicted_distrib)
+        show_distrib(real_distrib, predicted=False, ax=axs[0], limit = limit)
+        show_distrib(predicted_distrib, predicted=True, ax=axs[1], limit = limit)
+        fig.tight_layout()
+        plt.savefig(os.path.join(plots_folder_path,''+str(real_distrib.shape[1])+'dim_distribution.png'))
+        plt.close()
+
 
 
 